71021f17c8a07cd9787f3807e33cbe873fff4f31
Umoren <samuelumoren365@gmail.com>
2022-11-29 12:40:39 +0100

    made some changes

--- pages/image.js
+++ pages/image.js
@@ -12,6 +12,8 @@ const ImageDemo = ({ product }) => {
                         alt="layout Responsive"
                         src={pr.image}
                         width={700}
+                        placeholder="blur"
+                        blurDataURL={`data:application/xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48RXJyb3I+PENvZGU+SHR0cFZlcnNpb25Ob3RTdXBwb3J0ZWQ8L0NvZGU+PE1lc3NhZ2U+VGhlIEhUVFAgdmVyc2lvbiBzcGVjaWZpZWQgaXMgbm90IHN1cHBvcnRlZC48L01lc3NhZ2U+PFJlcXVlc3RJZD43MkQ4NUVCQkMxQjg3QUVGPC9SZXF1ZXN0SWQ+PEhvc3RJZD5FdWxFc05sTWVLYnBHNStSVlc1bWFFTWlENzJNQ1pCTW8zbytGWmJuVnBYVVJrV1RQZkxoZC9iSWpoa0pUWDJ3czBOSVJQQVcyNGY1U3BwdUNEVkQwK25qQVkvbDNsVDQ8L0hvc3RJZD48L0Vycm9yPg==`}
                         height={475}
                         sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw, 33vw"
                         style={{
